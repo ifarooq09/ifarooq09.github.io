ef9bf942e65d18b327a5b2bb8af46cdf0f0aa4a8
Ibrahim Farooq <ifarooq9@gmail.com>
2022-12-18 16:29:50 +0430

    3rd Day Changes

--- ReactSeries/beginner/src/Content.js
+++ ReactSeries/beginner/src/Content.js
@@ -1,16 +1,32 @@
-import React from 'react'
+import React from 'react';
+import { useState } from 'react';
 
 const Content = () => {
+    const [name, setName] = useState('Ibrahim');
+    const [count, setCount] = useState(0);
     const callingRandomNames = () => {
         const names = ['Ibrahim','Mustafa','Waseem'];
         const int = Math.floor(Math.random() * 3);
-        return names[int];
+        setName(names[int]);
       }
+
+    const handleClick = () => {
+      console.log(name)
+    }
+
+    const handleClick2 = () => {
+      setCount(count + 1)
+      console.log(count)
+    }
+
+        
   return (
     <main>
-        <p>
-          Hello {callingRandomNames()}!
+        <p onDoubleClick={handleClick}>
+          Hello {name}!
         </p>
+        <button onClick={callingRandomNames}>Clicke Here to Change Name</button>
+        <button onClick={ () => handleClick2 ('Ibrahim')}>Click Here for Counting</button>
     </main>
   )
 }
