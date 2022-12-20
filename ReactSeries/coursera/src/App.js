db70a2929747caaef124343b0e59bc9fbe05b062
Ibrahim Farooq <ifarooq9@gmail.com>
2022-12-20 16:42:31 +0430

    Components Part 1

--- ReactSeries/coursera/src/App.js
+++ ReactSeries/coursera/src/App.js
@@ -1,14 +1,16 @@
 import { Navbar, NavbarBrand } from 'reactstrap'; 
+import Menu from './components/MenuComponent';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div>
       <Navbar dark color='primary'>
         <div className='container'>
           <NavbarBrand href='/'>Afghan Deliciouse Food Restaurant</NavbarBrand>
         </div>
       </Navbar>
+      <Menu />
     </div>
   );
 }
